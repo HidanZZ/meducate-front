a37617f0596c70eecff5626e88006d328e458b95
ismail-nabaji <91204944+ismail-nabaji@users.noreply.github.com>
2023-05-05 15:11:16 +0200

    new card for even presentation

--- src/views/ui/cards/EventCard.tsx
+++ src/views/ui/cards/EventCard.tsx
@@ -1,41 +1,73 @@
-// ** React Imports
-import { useState } from 'react'
-
-// ** MUI Imports
-import Tab from '@mui/material/Tab'
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
-import TabList from '@mui/lab/TabList'
-import TabPanel from '@mui/lab/TabPanel'
 import Button from '@mui/material/Button'
-import TabContext from '@mui/lab/TabContext'
+import Avatar from '@mui/material/Avatar'
+import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
+import AvatarGroup from '@mui/material/AvatarGroup'
 
 
 const CardNavigationCenter = () => {
-  // ** State
-  const [value, setValue] = useState('1')
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
 
   return (
-    <Card>
-      <CardMedia sx={{ height: '9.375rem' }} image='/images/cards/watch-on-hand.jpg' />
-      <CardContent sx={{ p: theme => `${theme.spacing(3, 5.25, 4)} !important` }}>
-        <Typography variant='h6' sx={{ mb: 2 }}>
-          Apple Watch
-        </Typography>
-        <Typography sx={{ mb: 2 }}>$249.40</Typography>
-        <Typography variant='body2'>
-          3.1GHz 6-core 10th-generation Intel Core i5 processor, Turbo Boost up to 4.5GHz
-        </Typography>
+    <Card sx={{ position: 'relative', alignItems : 'center', justifyContent : 'center', margin : '0 auto', padding : '0 10' }}>
+      <CardMedia sx={{ height: '12.625rem' }} >
+        <img 
+          src='/images/icons/LOGO-M-V2.png' 
+          alt='Logo'
+          style={{ height: '100%', width: 'auto', alignItems : 'center', justifyContent : 'center', display : 'flex', margin : '0 auto', padding : '0 10'}} 
+        />
+      </CardMedia>      
+      <Avatar
+        alt='Robert Meyer'
+        src='/images/avatars/1.png'
+        sx={{
+          width: 75,
+          height: 75,
+          left: '1.313rem',
+          top: '10.28125rem',
+          position: 'absolute',
+          border: theme => `0.25rem solid ${theme.palette.common.white}`
+        }}
+      />
+      <CardContent>
+        <Box
+          sx={{
+            mt: 5.75,
+            mb: 8.75,
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Box sx={{ mr: 2, mb: 1, display: 'flex', flexDirection: 'column' }}>
+            <Typography variant='h6'>Robert Meyer</Typography>
+            <Typography variant='caption'>CEO</Typography>
+          </Box>
+          <Button variant='contained'>Join Webinar</Button>
+        </Box>
+        <Box sx={{ gap: 2, display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Typography variant='body1' sx={{ whiteSpace: 'wrap', color: 'text.primary', textAlign: 'center' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consectetur volutpat velit. Quisque ut lacinia diam. Maecenas rutrum tortor eget est facilisis eleifend. Aliquam vel dolor eget augue rutrum vulputate quis ut nibh. Aenean blandit justo sem, non scelerisque augue luctus vitae. Duis arcu sapien, commodo ut vulputate eu, viverra a tellus. Integer tincidunt, est at elementum posuere, lorem nibh tincidunt lacus, imperdiet vestibulum purus augue id erat. Fusce vestibulum felis at accumsan maximus. Duis laoreet aliquet ante, vel tincidunt ligula lobortis quis. Vestibulum a diam sed leo vehicula vulputate non vitae urna.
+          </Typography>
+        </Box>
+        <Box sx={{ gap: 2, display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Typography variant='subtitle2' sx={{ whiteSpace: 'nowrap', color: 'text.primary' }}>
+            Participants
+          </Typography>
+          <AvatarGroup max={4}>
+            <Avatar src='/images/avatars/8.png' alt='Alice Cobb' />
+            <Avatar src='/images/avatars/7.png' alt='Jeffery Warner' />
+            <Avatar src='/images/avatars/3.png' alt='Howard Lloyd' />
+            <Avatar src='/images/avatars/2.png' alt='Bettie Dunn' />
+            <Avatar src='/images/avatars/4.png' alt='Olivia Sparks' />
+            <Avatar src='/images/avatars/5.png' alt='Jimmy Hanson' />
+            <Avatar src='/images/avatars/6.png' alt='Hallie Richards' />
+          </AvatarGroup>
+        </Box>
       </CardContent>
-      <Button variant='contained' sx={{ py: 2.5, width: '100%', borderTopLeftRadius: 0, borderTopRightRadius: 0 }}>
-        Add To Cart
-      </Button>
     </Card>
 
   )
