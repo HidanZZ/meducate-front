ef5d0ecf874592338ba826454917fc5c85db38ac
ismail-nabaji <91204944+ismail-nabaji@users.noreply.github.com>
2023-05-09 12:54:23 +0200

    fixed layout of dialogue

--- src/views/ui/cards/SpeakerCard.tsx
+++ src/views/ui/cards/SpeakerCard.tsx
@@ -6,7 +6,6 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Box from '@mui/material/Box'
 import Dialog from '@mui/material/Dialog'
-import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import Fade from '@mui/material/Fade'
 import DialogContent from '@mui/material/DialogContent'
@@ -18,7 +17,7 @@ const Transition = forwardRef(function Transition(props, ref) {
   return <Fade ref={ref} {...props} />
 })
 
-const CardInfluencer = (props) => {
+const CardSpeaker = (props) => {
   const [show, setShow] = useState(false)
 
 
@@ -86,8 +85,7 @@ const CardInfluencer = (props) => {
         maxWidth='lg'
         onClose={() => setShow(false)}
         TransitionComponent={Transition}
-        BackdropProps={{ onClick: () => setShow(false) }}
-        >
+      >
         <DialogContent sx={{ px: { xs: 8, sm: 15 }, py: { xs: 8, sm: 12.5 }, position: 'relative' }}>
           <IconButton
             size='small'
@@ -96,48 +94,84 @@ const CardInfluencer = (props) => {
           >
             <Icon icon='mdi:close' />
           </IconButton>
-          <CardMedia sx={{ height: '14.625rem' }}>
-        {speaker.picture ? (
-          <img
-            src={speaker.picture}
-            alt='image'
-            style={{
-              height: '100%',
-              width: 'auto',
-              alignItems: 'center',
-              justifyContent: 'center',
+          <Box
+            sx={{
+              mt: 5.75,
+              ml: 4,
+              mr: 4,
               display: 'flex',
-              margin: '0 auto',
-              padding: '0 10',
-            }}
-          />
-        ) : (
-          <img
-            src='/images/avatars/1.png'
-            alt='Logo'
-            style={{
-              height: '100%',
-              width: 'auto',
               alignItems: 'center',
-              justifyContent: 'center',
-              display: 'flex',
-              margin: '0 auto',
-              padding: '0 10',
-              borderRadius: '25%',
+              flexDirection: ['column', null, 'row'],
             }}
-          />
-        )}
-      </CardMedia>
-          <Box sx={{ mb: 4, textAlign: 'center' }}>
-            <Typography variant='h5' sx={{ mb: 3 }}>
-              {speaker.firstName} {speaker.lastName}
-            </Typography>
-            <Typography variant='body2'>
-              {speaker.description}
-            </Typography>
+          >
+            <CardMedia
+              sx={{
+                height: '14.625rem',
+                width: ['100%', null, '25%'],
+                display: 'flex',
+              }}
+            >
+              {speaker.picture ? (
+                <img
+                  src={speaker.picture}
+                  alt='image'
+                  style={{
+                    height: '100%',
+                    width: 'auto',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    display: 'flex',
+                    margin: '0 auto',
+                    padding: '0 10',
+                  }}
+                />
+              ) : (
+                <img
+                  src='/images/avatars/1.png'
+                  alt='Logo'
+                  style={{
+                    height: '100%',
+                    width: 'auto',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    display: 'flex',
+                    margin: '0 auto',
+                    padding: '0 10',
+                    borderRadius: '25%',
+                  }}
+                />
+              )}
+            </CardMedia>
+            <Box
+              sx={{
+                m: 6,
+                width: ['100%', null, '70%'],
+                alignItems: ['center', null, 'flex-start'],
+                justifyContent: 'space-between',
+                ml: [0, null, 4],
+                mt: [4, null, 0],
+              }}
+            >
+              <Typography variant='h2' sx={{ mb: 0 }}>
+                {speaker.firstName} {speaker.lastName}
+              </Typography>
+              <Typography variant='h6' sx={{ ml: 0 }}>
+                {speaker.jobTitle}@{speaker.company}
+              </Typography>
+              <Typography variant='h4' sx={{ mt: 5, mb: 5, ml: 0 }}>
+                Bio
+              </Typography>
+              <Typography variant='body1' sx={{ ml: 0 }}>
+                {speaker.description}
+              </Typography>
+            </Box>
           </Box>
+
+
+          <Box sx={{ mt: 5.75, ml: 4, mr: 4, display: 'flex', alignItems: 'center' }}>
+            
+          </Box> 
          
-          
         </DialogContent>
       </Dialog>
 
@@ -147,4 +181,4 @@ const CardInfluencer = (props) => {
   );
 };
 
-export default CardInfluencer;
+export default CardSpeaker;
