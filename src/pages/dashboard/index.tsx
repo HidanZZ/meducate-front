95eee0aa38364d107888ce370924e410ffbea490
HidanZZ <aitdaoudelhoussein@gmail.com>
2023-05-26 12:06:50 +0100

    replaced search with autocomplete

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,7 @@
 import { Grid } from "@mui/material";
-import { AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
 
-import { useSelector } from "react-redux";
 import ApexChartWrapper from "src/@core/styles/libs/react-apexcharts";
-import { AppDispatch } from "src/store";
-import  { fetchNames, reset } from "src/store/apps/dashboard/components/searchNames";
-import SearchField from "src/views/forms/dashboard/SearchField";
-import NameCards from "src/views/pages/dashboard/NameCards";
+import AutocompleteComponent from "src/views/pages/dashboard/AutoComplete";
 import SentimentTrendChart from "src/views/pages/dashboard/SentimentTrendChart";
 import TopNamesBySentimentChart from "src/views/pages/dashboard/TopNamesBySentiment";
 import TopNamesChart from "src/views/pages/dashboard/TopNamesChart";
@@ -19,32 +12,13 @@ import WordcloudCard from "src/views/pages/dashboard/WordcloudCard";
 
 
 const Dashboard = () => {
-    const [searchValue, setSearchValue] = useState('');
-    const dispatch = useDispatch<AppDispatch>()
-
-    const {names  } = useSelector((state: any) => state.dashboard.searchNames);
-
-    useEffect(() => {
-        if (searchValue.length > 2) {
-            dispatch(fetchNames(searchValue));
-        }else{
-            dispatch(reset())
-        }
-    }, [searchValue, dispatch]);
+   
 
     return(
         <ApexChartWrapper>
             <Grid container spacing={6}>
                 <Grid item xs={12}  >
-                    <SearchField 
-                    inputValue={searchValue}
-                    setInputValue={setSearchValue}
-                    />
-                    </Grid>
-                    <Grid item xs={12}>
-          <AnimatePresence>
-            {names && names.length > 0 && <NameCards names={names} />}
-          </AnimatePresence>
+                    <AutocompleteComponent hidden={false}/>
         </Grid>
                 <Grid item xs={12} md={6} >
                     <TopNamesChart />
